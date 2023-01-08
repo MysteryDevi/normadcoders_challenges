ba612f0fd30a2b3010501a522a6d4c668dafa448
davidjeong03 <121111644+davidjeong03@users.noreply.github.com>
2023-01-09 02:52:38 +0900

    Update background.js

--- background.js
+++ background.js
@@ -2,4 +2,4 @@ const img = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
 const background = document.querySelector("#background-img");
 
 const randomImg = img[Math.floor(Math.random() * img.length)];
-background.style.backgroundImage = `url("images/${randomImg}")`;
+background.style.backgroundImage = `url("./images/${randomImg}")`;
